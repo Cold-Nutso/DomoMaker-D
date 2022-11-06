819ada3f4f882a470001f04643c4fce24e47a861
Cold-Nutso <vwp5337@rit.edu>
2022-11-06 16:40:36 -0500

    Finished 1-5 of assignment D.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const path = require('path');
 
 module.exports = {
     entry: './client/client.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
     mode: 'production',
     watchOptions: {
         aggregateTimeout: 200,
